ea9f632c1cd6984be4e44248a1629696ed4c61fa
Qiudan <dazzydan1998@gmail.com>
2022-11-14 16:20:32 +0800

    Crawl web

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request
+from scrape_baidu import Scrape_Baidu
+from scrape_sogou import Scrape_Sogou
+from scrape_wechat import Scrape_Wechat
+import time
+
+app = Flask(__name__, template_folder="templates")
+
+
+@app.route("/")
+def index():
+    return "Hello World"
+
+
+@app.route("/search-wd", methods=["GET", "POST"])
+def search_wd():
+    dict_concat = {}
+    baidu_dict = {}
+    sogou_dict = {}
+    wechat_dict = {}
+    if request.method == "POST":
+        search_wd = request.form["search_wd_input"]
+        search_page = request.form["search_page"]
+        search_method = request.form.getlist("search_method")
+        error = None
+        print(search_wd)
+        print(search_page)
+        if not search_wd:
+            error = "search_wd is required."
+        elif not search_page:
+            error = "search_page is required."
+        elif not search_method:
+            error = "search_method is required"
+
+        if error is None:
+            for i in search_method:
+                if i == "baidu":
+                    baidu = Scrape_Baidu(search_wd, search_page)
+                    baidu_dict = baidu.search()
+                    time.sleep(1)
+                elif i == "sogou":
+                    sogou = Scrape_Sogou(search_wd, search_page)
+                    sogou_dict = sogou.search()
+                    time.sleep(1)
+                elif i == "wechat":
+                    wechat = Scrape_Wechat(search_wd, search_page)
+                    wechat_dict = wechat.search()
+            dict_concat = {**baidu_dict, **sogou_dict, **wechat_dict}
+    return render_template("homepage.html", results=dict_concat)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ scrape_wechat.py
@@ -0,0 +1,124 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.common import exceptions
+from selenium.webdriver import ChromeOptions
+
+
+class Scrape_Wechat:
+    def __init__(self, search_word, max_page):
+        url = "https://weixin.sogou.com/"
+        self.url = url
+
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option(
+            "prefs", {"profile.managed_default_content_settings.images": 2}
+        )  # 不加载图片,加快访问速度
+        options.add_experimental_option(
+            "excludeSwitches", ["enable-automation"]
+        )  # 此步骤很重要，设置为开发者模式，防止被各大网站识别出来使用了Selenium
+        options.add_experimental_option("useAutomationExtension", False)
+        self.search_word = search_word
+        self.max_page = max_page
+        self.browser = webdriver.Chrome(
+            ChromeDriverManager().install(), options=options
+        )
+        self.wait = WebDriverWait(self.browser, 10)  # 超时时长为10s
+        self.result = {}
+
+    def search(self):
+        # 打开百度网页
+        self.browser.execute_cdp_cmd(
+            "Page.addScriptToEvaluateOnNewDocument",
+            {
+                "source": 'Object.defineProperty(navigator,"webdriver",{get:()=>undefind})'
+            },
+        )
+        self.browser.get(self.url)
+        # 等待搜索框出现，最多等待10秒，否则报超时错误
+        search_input = self.wait.until(
+            EC.presence_of_element_located((By.XPATH, '//*[@id="query"]'))
+        )
+        # 在搜索框输入搜索的关键字
+        search_input.send_keys(self.search_word)
+        # 回车
+        search_input.send_keys(Keys.ENTER)
+        # 等待10秒钟
+        self.browser.implicitly_wait(10)
+
+        result_dict = self.save_results()
+        self.tear_down()
+        return result_dict
+
+    def save_results(self):
+        # 找到所有的搜索结果
+        s = 0
+        while True:
+            s += 1
+            try:
+                results = self.browser.find_elements(
+                    By.CSS_SELECTOR, ".txt-box > h3 > a"
+                )
+                for result in results:
+                    if result.get_attribute("href"):
+                        # 搜索结果的标题
+                        title = result.text.strip()
+                        # 搜索结果的网址
+                        link = result.get_attribute("href")
+                        if link not in self.result.keys():
+                            self.result[link] = title
+
+            except exceptions.StaleElementReferenceException as e:
+                print("查找元素异常")
+                print("重新获取元素")
+                results = self.browser.find_elements(
+                    By.CSS_SELECTOR, ".txt-box > h3 > a"
+                )
+                for result in results:
+                    if result.get_attribute("href"):
+                        # 搜索结果的标题
+                        title = result.text.strip()
+                        # 搜索结果的网址
+                        link = result.get_attribute("href")
+                        if link not in self.result.keys():
+                            self.result[link] = title
+
+            next_page_exist = self.next_page()
+            print(f"Scraping {s}/{self.max_page}...")
+            if next_page_exist is False or int(s) == int(self.max_page):
+                break
+
+        return self.result
+
+    def next_page(self):
+        try:
+            if len(self.browser.find_elements(By.ID, "sogou_next")) > 0:
+                i = self.browser.find_element(By.ID, "sogou_next")
+                self.wait.until(EC.element_to_be_clickable(i)).click()
+                return True
+            else:
+                print("Next page doesn't exist")
+                return False
+        except exceptions.StaleElementReferenceException as e:
+            print("查找下一页按键元素异常")
+            print("重新获取元素")
+            if len(self.browser.find_elements(By.ID, "sogou_next")) > 0:
+                i = self.browser.find_element(By.ID, "sogou_next")
+                self.wait.until(EC.element_to_be_clickable(i)).click()
+                return True
+            else:
+                print("Next page doesn't exist")
+                return False
+
+    def tear_down(self):
+        self.browser.close()
+
+
+if __name__ == "__main__":
+    search_word = "selenium"
+    max_page = 3
+    search = Scrape_Wechat(search_word, max_page)
+    df_wechat = search.search()
